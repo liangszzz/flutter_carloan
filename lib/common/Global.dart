894dcfe52f93b30dfa512a2dde3f93e0fb55b7cd
yiyan1992 <liang-shan@outlook.com>
2019-02-12 16:19:54 +0800

    global info

--- lib/common/Global.dart
+++ lib/common/Global.dart
@@ -1,23 +1,40 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter_carloan/common/Token.dart';
 import 'package:flutter_carloan/common/User.dart';
 
 ///全局共享信息
 class Global {
-  
   Token token;
 
   User user;
+
+  static Dio dio = Dio(Options(
+    baseUrl: "http://192.168.1.12:8081/",
+    connectTimeout: 10000,
+    receiveTimeout: 100000,
+    contentType: ContentType.json,
+  ));
 
   factory Global() => _getInstance();
 
   static Global _instance;
 
-  Global._internal() {}
+  Global._internal();
 
   static Global _getInstance() {
     if (_instance == null) {
       _instance = new Global._internal();
     }
     return _instance;
+  }
+
+  Future<Response> dioPost(String url, [Map params]) {
+    if (token != null) {
+      dynamic tok = token.token;
+      dio.options.headers.putIfAbsent("token", tok);
+    }
+    return dio.post(url, data: params);
   }
 }
--- lib/common/User.dart
+++ lib/common/User.dart
@@ -1,5 +1,26 @@
 
-class User{
+class User {
+  ///昵称
+  String nickName;
 
+  ///头像
+  String avatarUrl;
 
+  ///手机号
+  String phone;
+
+  ///身份证号
+  String idcard;
+
+  ///银行名称
+  String bankName;
+
+  ///银行卡号 后四位
+  String barkCard;
+
+  ///token
+  String token;
+
+  ///token过期时间
+  String expire;
 }
