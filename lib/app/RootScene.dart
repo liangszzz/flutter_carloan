28582ab2f4530184f63811d41c19f21f81a1211e
Walkerontheway <1521481790@qq.com>
2019-02-14 15:45:26 +0800

    底部栏

---
+++ lib/app/RootScene.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_carloan/common/Global.dart';
+import 'package:flutter_carloan/login/LoginPage.dart';
+import 'package:flutter_carloan/me/MeScene.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class RootScene extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => RootSceneState();
+}
+
+class RootSceneState extends State<RootScene> {
+  int _tabIndex = 1;
+  bool isFinishSetup = false;
+  List<Image> _tabImages = [
+    Image.asset('img/tab_bookshelf_n.png'),
+    Image.asset('img/tab_me_n.png'),
+  ];
+  List<Image> _tabSelectedImages = [
+    Image.asset('img/tab_bookshelf_p.png'),
+    Image.asset('img/tab_me_p.png'),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    setupApp();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  setupApp() async {
+    preferences = await SharedPreferences.getInstance();
+    setState(() {
+      isFinishSetup = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!isFinishSetup) {
+      return Container();
+    }
+
+    return Scaffold(
+      body: IndexedStack(
+        children: <Widget>[
+          LoginPage(),
+          MeScene(),
+        ],
+        index: _tabIndex,
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: Colors.white,
+        activeColor: Colors.red,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: getTabIcon(0), title: Text('借款记录')),
+          BottomNavigationBarItem(icon: getTabIcon(1), title: Text('我的')),
+        ],
+        currentIndex: _tabIndex,
+        onTap: (index) {
+          setState(() {
+            _tabIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  Image getTabIcon(int index) {
+    if (index == _tabIndex) {
+      return _tabSelectedImages[index];
+    } else {
+      return _tabImages[index];
+    }
+  }
+}
