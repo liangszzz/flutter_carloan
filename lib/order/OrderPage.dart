d3107edeb1f1280aa7acb71aa4f42a1bc885fc5a
yiyan1992 <liang-shan@outlook.com>
2019-02-18 10:16:34 +0800

    修改类名

--- lib/order/OrderPage.dart
+++ lib/order/OrderPage.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
-class order extends StatelessWidget{
-
+class OrderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Center(
       child: new Text("order info"),
     );
   }
-
 }
