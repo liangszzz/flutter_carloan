6544eea54767188b956e48a57af325320c5f7051
DOFORME <15501673636@163.com>
2019-02-20 09:56:46 +0800

    更新实体类

--- lib/my_bank_card/my_bank_card.dart
+++ lib/my_bank_card/my_bank_card.dart
@@ -1,36 +1,11 @@
 
 
 class MyBankCard {
-  String _idCard;
+  String idCard;
 
-  String _bankName;
+  String bankName;
 
-  String _signDate;
+  String signDate;
 
-  String _cardNo;
-
-  String get idCard => _idCard;
-
-  set idCard(String value) {
-    _idCard = value;
-  }
-
-  String get bankName => _bankName;
-
-  String get cardNo => _cardNo;
-
-  set cardNo(String value) {
-    _cardNo = value;
-  }
-
-  String get signDate => _signDate;
-
-  set signDate(String value) {
-    _signDate = value;
-  }
-
-  set bankName(String value) {
-    _bankName = value;
-  }
-
+  String cardNo;
 }
