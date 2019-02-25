408de9709b8791cd5b013aff56e4205f420bfd2b
Walkerontheway <1521481790@qq.com>
2019-02-25 11:00:07 +0800

    协议页面

--- lib/agreeMent/AgreeMentPageInfo.dart
+++ lib/agreeMent/AgreeMentPageInfo.dart
@@ -1,60 +1,60 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_carloan/common/Global.dart';
 import 'package:flutter_html_textview/flutter_html_textview.dart';
 
-class HtmlInfoPage extends StatefulWidget {
+class AgreementInfoPage extends StatefulWidget {
+  final String title;
+  final String bizOrderNo;
+
+  ///1 表示正式表 2 表示微信表
+  final int channelType;
+
+  const AgreementInfoPage({Key key, this.title, this.channelType, this.bizOrderNo}) : super(key: key);
+
   @override
-  _MyAppState createState() => new _MyAppState();
+  _AgreementInfoState createState() => new _AgreementInfoState();
 }
 
-class _MyAppState extends State<HtmlInfoPage> {
+class _AgreementInfoState extends State<AgreementInfoPage> {
   @override
   initState() {
     super.initState();
     initPlatformState();
   }
 
-  String lorem ="";
+  String htmlInfo = "";
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   initPlatformState() async {}
 
   bool isReload = false;
 
   @override
   Widget build(BuildContext context) {
-
-
     _getAgree();
-
-
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Plugin example app'),
-        ),
-        body: new SingleChildScrollView(
-          child: new Center(
-            child: new HtmlTextView(
-              data: lorem,
-            ),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new SingleChildScrollView(
+        child: new Center(
+          child: new HtmlTextView(
+            data: htmlInfo,
           ),
         ),
       ),
     );
   }
 
+  Global global = Global();
   Future _getAgree() async {
-    if(!isReload){
+    if (!isReload) {
       isReload = true;
       String content = "";
-      await Dio().post("http://192.168.1.13:8081/agreement/query/QSM20190222111810and/1").then((response) {
-        content = response.data["msg"];
-      });
+      var response = await global.postFormData("agreement/query/"+widget.bizOrderNo + "/" + widget.channelType.toString());
+      content = response["msg"];
       setState(() {
-        lorem = content;
+        htmlInfo = content;
       });
     }
   }
 }
-
