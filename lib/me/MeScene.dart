2c7ee562f73b7fcadfd9eca29e743666729d9e58
Walkerontheway <1521481790@qq.com>
2019-02-28 16:50:00 +0800

    修改传值

--- lib/me/MeScene.dart
+++ lib/me/MeScene.dart
@@ -112,7 +112,7 @@ class _MeSceneState extends State<_MeSceneStateful> {
             iconName: 'assets/images/message.png',
             onPressed: () {
               Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return MyMessage(phone: '13770207216');
+                return MyMessage(phone: global.user.phone);
               }));
             },
           ),
