2a6a47b692c9c86efde5aac227e4c10ebad7f519
yiyan1992 <liang-shan@outlook.com>
2019-06-13 17:23:08 +0800

    配置信息

--- lib/app/Global.dart
+++ lib/app/Global.dart
@@ -11,72 +11,74 @@ SharedPreferences preferences;
 
 ///全局共享信息
 class Global {
-  Token token;
+    Token token;
 
-  User user;
+    User user;
 
 
 
-  String currentVersion = "1.0.0";
+    String currentVersion = "1.0.0";
 
-  ThemeData globalTheme = ThemeData(
-    primaryColor: Colors.white,
-  );
+    ThemeData globalTheme = ThemeData(
+        primaryColor: Colors.white,
+    );
 //http://106.15.126.226:8081/
-  Dio dio = new Dio(BaseOptions(baseUrl: "http://106.15.126.226:8081/"));
+//  Dio dio = new Dio(BaseOptions(baseUrl: "http://192.168.1.12:8081/mobile/"));
+//  Dio dio = new Dio(BaseOptions(baseUrl: "http://106.15.126.226:8081/mobile/"));
+    Dio dio = new Dio(BaseOptions(baseUrl: "http://carloan-manage.qsmartec.com/mobile/"));
 
-  /// 0 安卓 1,IOS 2.windows 3.
-  int DEVICE = 0;
+    /// 0 安卓 1,IOS 2.windows 3.
+    int DEVICE = 0;
 
-  HttpClient httpClient = new HttpClient();
+    HttpClient httpClient = new HttpClient();
 
-  ///验证码倒计时时间
-  final int SECOND = 60;
+    ///验证码倒计时时间
+    final int SECOND = 60;
 
-  factory Global() => _getInstance();
+    factory Global() => _getInstance();
 
-  static Global _instance;
+    static Global _instance;
 
-  Global._internal();
+    Global._internal();
 
-  static Global _getInstance() {
-    if (_instance == null) {
-      _instance = new Global._internal();
+    static Global _getInstance() {
+        if (_instance == null) {
+            _instance = new Global._internal();
+        }
+        return _instance;
     }
-    return _instance;
-  }
 
-  Future<Map> post(String url, [queryParameters]) async {
-    if (token != null && token.token != null) {
-      dio.options.headers.putIfAbsent("token", () => token.token);
-    } else {
-      dio.options.headers.remove("token");
-    }
-    Response responseBody =
+    Future<Map> post(String url, [queryParameters]) async {
+        if (token != null && token.token != null) {
+            dio.options.headers.putIfAbsent("token", () => token.token);
+        } else {
+            dio.options.headers.remove("token");
+        }
+        Response responseBody =
         await dio.post(url, queryParameters: queryParameters);
-    return responseBody.data;
-  }
-
-  Future<Map> postFormData(String url, [data]) async {
-    if (token != null && token.token != null) {
-      dio.options.headers.putIfAbsent("token", () => token.token);
-    } else {
-      dio.options.headers.remove("token");
+        return responseBody.data;
     }
-    Response responseBody = await dio.post(url, data: data);
-    return responseBody.data;
-  }
 
-  void loadTokenAndUserInfo(DataResponse d) {
-    Map<String, dynamic> map = d.entity as Map;
-    //将用户信息写入global
-    User user = User.fromJson(map);
-    this.user = user;
-    //将token信息写入global
-    if (map['expire'] != null && map['token'] != null) {
-      Token token = Token.parseToken(map['expire'] + map['token']);
-      token.writeToken();
-      this.token = token;
+    Future<Map> postFormData(String url, [data]) async {
+        if (token != null && token.token != null) {
+            dio.options.headers.putIfAbsent("token", () => token.token);
+        } else {
+            dio.options.headers.remove("token");
+        }
+        Response responseBody = await dio.post(url, data: data);
+        return responseBody.data;
     }
-  }
+
+    void loadTokenAndUserInfo(DataResponse d) {
+        Map<String, dynamic> map = d.entity as Map;
+        //将用户信息写入global
+        User user = User.fromJson(map);
+        this.user = user;
+        //将token信息写入global
+        if (map['expire'] != null && map['token'] != null) {
+            Token token = Token.parseToken(map['expire'] + map['token']);
+            token.writeToken();
+            this.token = token;
+        }
+    }
 }
